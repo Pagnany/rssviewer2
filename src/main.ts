848fc3b1a21ab35f36a3fc4257b154096996db73
Pagnany <eric.schmale@gmx.de>
2026-04-24 07:30:26 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { invoke } from "@tauri-apps/api/core";
+import { fetch } from '@tauri-apps/plugin-http';
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
+
+const response = await fetch('https://www.google.com/', {
+  method: 'GET',
+});
+console.log(response.status); // e.g. 200
+console.log(response.statusText); // e.g. "OK"
