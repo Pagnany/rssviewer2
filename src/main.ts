ca958d427455bc6c45479f654f8071ecc3d1bf95
Pagnany <eric.schmale@gmx.de>
2026-04-25 12:11:29 +0200

    index

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,35 @@ window.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#rss-refresh")!.addEventListener("click", async (e) => {
     e.preventDefault();
 
-    invoke('get_rssfeed_channels').then((result: any) => {
-      rssFeedD1!.textContent = result;
+    invoke('get_rssfeeds').then((message: any) => {
+      rssFeedD1!.textContent = message;
+      let rssDate = "";
+      message.forEach((item: any) => {
+        rssDate += "<article>";
+        rssDate += "<p>" + item.feed_name + "</p>";
+        rssDate += "<h4>" + item.header + "</h4>";
+        rssDate += "<p>" + item.description + "</p>";
+        rssDate += "<img src='" + item.image + "'/>" + "<br />";
+
+        rssDate +=
+          '<a href="' +
+          item.url +
+          ' " target="_blank">' +
+          "Link" +
+          "</a>" +
+          "<br />";
+
+        rssDate += item.date + "<br />";
+        rssDate += "</article>";
+        rssDate += "<br/>";
+      });
+
+      rssFeedD1!.innerHTML = rssDate;
     })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        rssFeedD1!.textContent = error;
+        console.error(error)
+      });
   });
 
   // Go to Top Button Click
