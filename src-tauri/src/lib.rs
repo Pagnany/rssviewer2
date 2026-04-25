5b48a7dbf5dc8a8c36fd0e1a80cb0677968f6582
Pagnany <eric.schmale@gmx.de>
2026-04-25 11:44:45 +0200

    fetch rssfeeds; parse with parse logic from rssviewerV1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use chrono::{DateTime, Local, Utc};
+use futures::{stream, SinkExt, StreamExt};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use tauri_plugin_http::reqwest;
 use tauri_plugin_http::reqwest::header::USER_AGENT;
@@ -11,7 +14,7 @@ pub fn run() {
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![test_http, get_rssfeed_channels])
+        .invoke_handler(tauri::generate_handler![get_rssfeeds])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -36,22 +39,60 @@ pub struct RssFeedChannel {
 }
 
 #[tauri::command]
-async fn test_http(url: &str) -> Result<String, String> {
-    let client = reqwest::Client::new();
-    let res = client
-        .get(url)
-        .header(USER_AGENT, "Rssviewer2/0.0.0")
-        .send()
-        .await
-        .map_err(|e| e.to_string())?;
+async fn get_rssfeeds() -> Result<String, String> {
+    // Only active channels
+    let channels = get_rssfeed_channels()
+        .await?
+        .into_iter()
+        .filter(|c| c.active == 1)
+        .collect::<Vec<_>>();
 
-    let body = res.text().await.map_err(|e| e.to_string())?;
-    println!("Response: {}", body);
-    Ok(body)
+    // Concurrent fetching of RSS feed channels
+    let client = reqwest::Client::new();
+    let concurrency = 10;
+
+    let results: Vec<Result<String, String>> = stream::iter(channels)
+        .map(|channel| {
+            let client = client.clone();
+            async move {
+                let res = client
+                    .get(&channel.url)
+                    .header(USER_AGENT, "Rssviewer2/0.0.0")
+                    .send()
+                    .await
+                    .map_err(|e| e.to_string())?;
+
+                let body = res.text().await.map_err(|e| e.to_string())?;
+                println!("Fetched {}: {} bytes", channel.name, body.len());
+                // return body
+                Ok(body)
+            }
+        })
+        .buffer_unordered(concurrency)
+        .collect()
+        .await;
+
+    // Filter and parse
+    let rssfeeditems: Vec<RssFeed> = results
+        .into_iter()
+        .filter_map(|res| match res {
+            Ok(feed) => Some(get_items_form_feed(&feed)),
+            Err(e) => {
+                eprintln!("Error fetching feed: {}", e);
+                None
+            }
+        })
+        .flatten()
+        .collect();
+
+    for item in rssfeeditems.iter().clone() {
+        println!("{} - {}", item.header, item.url);
+    }
+
+    Ok(format!("{:?}", rssfeeditems))
 }
 
-#[tauri::command]
-async fn get_rssfeed_channels() -> Result<String, String> {
+async fn get_rssfeed_channels() -> Result<Vec<RssFeedChannel>, String> {
     let url = "https://www.pagnany.de/api/rss.php";
     let params = [("action", "get_channels")];
     let client = reqwest::Client::new();
@@ -64,5 +105,59 @@ async fn get_rssfeed_channels() -> Result<String, String> {
 
     let body = res.text().await.map_err(|e| e.to_string())?;
     let channels: Vec<RssFeedChannel> = serde_json::from_str(&body).map_err(|e| e.to_string())?;
-    Ok(format!("{:?}", channels))
+    Ok(channels)
+}
+
+fn get_items_form_feed(feed: &str) -> Vec<RssFeed> {
+    let mut rss_feed_vec = Vec::new();
+
+    let doc = roxmltree::Document::parse(feed).unwrap();
+    let mut rss_feed_name = String::from("");
+    let mut rss_feed_name_set = false;
+    for node in doc.descendants() {
+        if node.tag_name().name() == "item" {
+            let mut rss_feed: RssFeed = Default::default();
+
+            for child in node.children() {
+                match child.tag_name().name() {
+                    "guid" => rss_feed.id = child.text().unwrap().to_string(),
+                    "title" => rss_feed.header = child.text().unwrap().to_string(),
+                    "description" => rss_feed.description = child.text().unwrap().to_string(),
+                    "link" => rss_feed.url = child.text().unwrap().to_string(),
+                    "pubDate" => {
+                        let utc_dt: DateTime<Utc> =
+                            DateTime::parse_from_rfc2822(child.text().unwrap())
+                                .unwrap()
+                                .with_timezone(&Utc);
+                        let date = utc_dt.with_timezone(&Local);
+                        rss_feed.date = date.format("%Y-%m-%d %H:%M").to_string();
+                    }
+                    "enclosure" => {
+                        if child.attribute("type").unwrap() == "image/jpeg" {
+                            rss_feed.image = child.attribute("url").unwrap().to_string();
+                        }
+                    }
+                    "encoded" => {
+                        // find <img> tag in a non xml string and get the link in src=""
+                        let content_encoded = child.text().unwrap().to_string();
+                        let re = Regex::new(r#"<img src="([^"]*)""#).unwrap();
+                        let caps = re.captures(&content_encoded);
+
+                        if let Some(caps) = caps {
+                            let img_src = &caps[1];
+                            rss_feed.image = img_src.to_string();
+                        }
+                    }
+                    _ => (),
+                }
+            }
+            rss_feed.feed_name = rss_feed_name.clone();
+            rss_feed_vec.push(rss_feed);
+        } else if node.tag_name().name() == "title" && !rss_feed_name_set {
+            rss_feed_name = node.text().unwrap().to_string();
+            rss_feed_name_set = true;
+        }
+    }
+
+    rss_feed_vec
 }
